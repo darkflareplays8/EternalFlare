137c9bc40600ed0de29695ba76ee30a3123c1c03
DarkFlare <saimuralibalmuri@gmail.com>
2025-05-22 07:58:05 +0100

    Update licence.js

--- commands/licence.js
+++ commands/licence.js
@@ -2,17 +2,14 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('licence')
-    .setDescription('Get a link to the bot's license.'),
+    .setName('license')
+    .setDescription("Get a link to the bot's license."),
 
   async execute(interaction) {
     const embed = new EmbedBuilder()
-      .setTitle('Liscence Link')
+      .setTitle('License Information')
       .setDescription(
-        `This bot is liscenced under MIT \n` +
-        `Click here to view full`
-
-        
+        `This bot is licensed under the MIT License.\n[Click here to view the full license](https://opensource.org/licenses/MIT)`
       )
       .setColor('#FF4500')
       .setTimestamp()
