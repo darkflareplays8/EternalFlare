ef03d74ac8087cfcf56f6e38c21c4a1dd45a80c6
DarkFlare <saimuralibalmuri@gmail.com>
2025-05-22 20:06:59 +0100

    fix meme syntax text: error

--- commands/meme.js
+++ commands/meme.js
@@ -1,13 +1,13 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const fetch = require('node-fetch'); // If you're using Node 18+, fetch is built-in
+const fetch = require('node-fetch'); // Only needed if < Node 18
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('meme')
     .setDescription('Get your random meme here!'),
-  
+
   async execute(interaction) {
-    await interaction.deferReply(); // In case API is slow
+    await interaction.deferReply();
 
     try {
       const response = await fetch('https://meme-api.com/gimme');
@@ -17,8 +17,8 @@ module.exports = {
         .setTitle("Your meme:")
         .setURL(meme.postLink)
         .setImage(meme.url)
-        .setColor(0xff9900)
-        .setFooter("EternalFlare Discord");
+        .setColor(#FF4500)
+        .setFooter({ text: "EternalFlare Discord" });
 
       await interaction.editReply({ embeds: [embed] });
 
