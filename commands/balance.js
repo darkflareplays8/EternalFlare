f2e91adee8df9bc6e18599ffb4134ee334edaf34
DarkFlare <saimuralibalmuri@gmail.com>
2025-05-25 10:43:40 +0100

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -10,7 +10,7 @@ module.exports = {
       host: process.env.MYSQLHOST,
       user: process.env.MYSQLUSER,
       password: process.env.MYSQLPASSWORD,
-      database: process.env.MYSQLDATABASE,
+      database: process.env.MYSQL_DATABASE,
       port: 3306
     });
 
