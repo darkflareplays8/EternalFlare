8d59553a326370c0f7127421088b09ca723c9a17
DarkFlare <saimuralibalmuri@gmail.com>
2025-05-25 09:12:50 +0100

    Create db.js

---
+++ db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,                // not MYSQL_HOST
+  user: process.env.MYSQLUSER,                // not MYSQL_USER
+  password: process.env.MYSQLPASSWORD,        // not MYSQL_PASSWORD
+  database: process.env.MYSQL_DATABASE,
+  port: 3306, // most likely, unless Railway says otherwise
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;
